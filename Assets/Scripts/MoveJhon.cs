2e8ece6d7a6585d24bf5966ff50d0bfadb80d7c6
Leonardo Berrio <leonardo.br4@hotmail.com>
2022-09-28 11:23:39 -0500

    Character movement update

--- Assets/Scripts/MoveJhon.cs
+++ Assets/Scripts/MoveJhon.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MoveJhon : MonoBehaviour
 {
+    public float JumpForce;
+    public float Speed = 1;
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
 
@@ -17,10 +19,29 @@ public class MoveJhon : MonoBehaviour
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(Horizontal < 0.0f)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        else if (Horizontal > 0.0f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
 
     private void FixedUpdate()
     {
-        Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
     }
 }
